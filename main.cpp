86c307672fddba62f4bac46c84ab3f15f5fc4fd9
Muhammad Rizal <112866261+Muhammadrizaaal@users.noreply.github.com>
2022-09-12 20:45:38 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Muhammad Rizal" << endl;
-    cout << "A11.2022.14127" << endl;
-    cout << "Soka Lerep RT02/04 Ungaran Barat" << endl;
-    cout << "SMAN 2 Ungaran" << endl;
+    cout << "<=======================================>" << endl;
+    cout << endl;
+    printf ("-)Muhammad Rizal");
+    cout << endl;
+    //cout
+    //printf
+    //int
+    // atau bisa menggunakan
+
+    /*
+    cout
+    printf
+    int
+    char
+    float
+    string
+    */
+
+    int X = 19; //int= integer,untuk bilangan bulat
+    float T = 171.5; // untuk bilangan rill seperti desimal
+    char B = 'L';
+    string AB = "XL";
+    string BC = "Extra Large";
+
+    cout << "-)Umur Saya: " << X;
+    cout << endl;
+    cout << "-)Tinggi Badan: " << T;
+    cout << endl;
+    cout << "-)Ukuran Baju: " << B;
+    cout << endl;
+    cout << "-)Ukuran Baju ke-2: " << AB;
+    cout << endl;
+    cout << "-)Ukuran Baju ke-2: " << BC;
+    cout << endl;
+    cout << endl;
+    cout << "<=======================================>" << endl;
     return 0;
 }
