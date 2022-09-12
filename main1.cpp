519c3a0abc5a6731d6c3509f9b69a3a59765ab72
Muhammad Rizal <112866261+Muhammadrizaaal@users.noreply.github.com>
2022-09-12 20:54:48 +0700

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Muhammad Rizal" << endl;
+    cout << "A11.2022.14127" << endl;
+    cout << "Soka Lerep RT02/04 Ungaran Barat" << endl;
+    cout << "SMAN 2 Ungaran" << endl;
+    return 0;
+}
